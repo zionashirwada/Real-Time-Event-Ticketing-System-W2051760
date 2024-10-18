db7de584c5b62c6bd1435d0df1206c8c3c843994
Ashirwada Manamudali <zionashirwada@users.noreply.github.com>
2024-10-18 19:13:09 +0530

    setup CORS

--- frontend/src/app/components/configuration-form/configuration-form.component.ts
+++ frontend/src/app/components/configuration-form/configuration-form.component.ts
@@ -1,24 +1,48 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { ConfigurationService } from '../../services/configuration.service';
+import { Configuration } from '../../types/configuration.type';
 
 @Component({
   selector: 'app-configuration-form',
   standalone: true,
-  imports: [CommonModule, FormsModule], 
+  imports: [],
   templateUrl: './configuration-form.component.html',
-  styleUrls: ['./configuration-form.component.css']
+  styleUrls: ['./configuration-form.component.css'],
 })
-export class ConfigurationFormComponent {
-  config = {
+export class ConfigurationFormComponent implements OnInit {
+  config: Configuration = {
     totalTickets: 0,
     ticketReleaseRate: 0,
     customerRetrievalRate: 0,
     maxTicketCapacity: 0,
   };
 
+  constructor(private configurationService: ConfigurationService) {}
+
+  ngOnInit() {
+    // Fetch the saved configs
+    this.configurationService.getConfiguration().subscribe({
+      next: (data) => {
+        if (data) {
+          this.config = data; 
+        }
+      },
+      error: (err) => {
+        console.error('Error fetching configuration:', err);
+      },
+    });
+  }
+
   onSubmit() {
-    console.log('Configuration submitted:', this.config);
-    alert('Configuration saved successfully!');
+    this.configurationService.setConfiguration(this.config).subscribe({
+      next: (response) => {
+        console.log('Configuration saved:', response);
+        alert('Configuration saved successfully!');
+      },
+      error: (err) => {
+        console.error('Error saving configuration:', err);
+        alert('Error saving configuration.');
+      },
+    });
   }
 }
