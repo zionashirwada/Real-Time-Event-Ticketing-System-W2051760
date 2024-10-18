cc69c0547c67710b9574a917dae51136cd5229dd
Ashirwada Manamudali <zionashirwada@users.noreply.github.com>
2024-10-18 16:26:12 +0530

    Backend Setup

---
+++ backend/src/main/java/lk/W2051760/ticketing_system_backend/TicketingSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package lk.W2051760.ticketing_system_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketingSystemBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/lk/W2051760/ticketing_system_backend/TicketingSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.W2051760.ticketing_system_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
